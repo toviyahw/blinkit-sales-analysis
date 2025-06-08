1808c1d9be2c6553acdb43783f100fbdd1442299
Toviyah Williams <toviyahwilliams@gmail.com>
2025-06-07 20:04:37 -0500

    update campaign and revenue images

--- sql_files/analysis/cost_per_conversion_by_audience.sql
+++
@@ -1,6 +0,0 @@
-SELECT target_audience,
-       ROUND(SUM(spend) / NULLIF(SUM(conversions), 0), 2) AS cost_per_conversion
-FROM marketing_performance
-GROUP BY target_audience
-ORDER BY cost_per_conversion ASC;
-
--- sql_files/analysis/cutomer_acquisition_cost_by_channel.sql
+++
@@ -1,6 +0,0 @@
-SELECT channel,
-       ROUND(SUM(spend) / NULLIF(SUM(conversions), 0), 2) AS customer_aqc_cost
-FROM marketing_performance
-GROUP BY channel
-ORDER BY customer_aqc_cost ASC;
-
